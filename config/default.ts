280d35cac966586ffb46f1a819ea09944a83181b
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2023-11-09 12:04:27 +0100

    Added Socket configuration

--- config/default.ts
+++ config/default.ts
@@ -4,10 +4,13 @@ dotenv.config()
 
 export default {
   port: 8080,
+  socketPort: 80,
   dbUri: process.env.DB_URI,
   saltWorkFactor: 10,
   accessTokenTimeToLive: '15m',
   refreshTokenTimeToLive: '1y',
   privateKey: process.env.PRIVATE_KEY,
-  publicKey: process.env.PUBLIC_KEY
+  publicKey: process.env.PUBLIC_KEY,
+  upstashRedisRestUrl: process.env.UPSTASH_REDIS_REST_URL,
+  origin: process.env.ORIGIN
 }
