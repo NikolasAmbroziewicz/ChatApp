9fc5c6a25b0a70ae962c91310f24d0f7c4dfcd00
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2023-12-14 12:03:05 +0100

    Added Message Component

--- frontend/src/features/Chat/components/Message.tsx
+++ frontend/src/features/Chat/components/Message.tsx
@@ -1,19 +1,29 @@
+import { USER_NAME, BOT_NAME } from '@/consts'
+
 interface IMessage {
+  type: string,
   author: string,
   date: string,
-  description: string,
+  message: string,
   isAuthor: boolean
 }
 
-const Message: React.FC<IMessage> = ({ author, date, description, isAuthor }) => {
+const Message: React.FC<IMessage> = ({ type, author, date, message, isAuthor }) => {
+
   return (
-    <div className={`flex flex-col justify-end w-[45%] bg-blue-200 ${isAuthor && 'ml-auto'} p-2 rounded`}>
-      <div className="flex items-center justify-between gap-2">
-        <span className="text-slate-700 text-xs">{date}</span>
-        <span className="text-slate-600">{author}</span>
+    type === BOT_NAME ? (
+      <span className='w-[100%] text-center p-2 rounded text-neutral-300'>
+        {message}
+      </span>
+    ) : (
+      <div className={`flex flex-col justify-end w-[45%] bg-blue-200 ${isAuthor && 'ml-auto'} p-2 rounded`}>
+        <div className="flex items-center justify-between gap-2">
+          <span className="text-slate-700 text-xs">{date}</span>
+          <span className="text-slate-600">{author}</span>
+        </div>
+        <span className="text-slate-950">{message}</span>
       </div>
-      <span className="text-slate-950">{description}</span>
-    </div>
+    )
   )
 }
 
