cef75b2cf3ee0f7fd6d779e7a9ddedaeda9a86dd
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2023-12-20 09:24:10 +0100

    Remove Endpoint responsible for getAllMessages and moved all logic to socket

--- src/modules/message/message.routes.ts
+++ src/modules/message/message.routes.ts
@@ -5,14 +5,13 @@ import requireUser from '../../middleware/requireUser'
 
 import {
   deleteMessageController,
-  getMessagesController,
   updateMessageController,
 } from './message.controller'
 
-import { chatParams, deleteMessageParams, updateMessagePayload } from './message.schema'
+import { deleteMessageParams, updateMessagePayload } from './message.schema'
 
 export function routes(app: Express) {
-  app.get('/v1/api/message/:chatId', [requireUser, validateResource(chatParams)], getMessagesController)
+
   app.put('/v1/api/message/:messageId', [requireUser, validateResource(updateMessagePayload)], updateMessageController)
   app.delete('/v1/api/message/:messageId', [requireUser, validateResource(deleteMessageParams), deleteMessageController])
 }
--- src/modules/message/message.schema.ts
+++ src/modules/message/message.schema.ts
@@ -1,13 +1,5 @@
 import { TypeOf, object, string } from 'zod'
 
-export const chatParams = object({
-  params: object({
-    chatId: string({
-      required_error: "productId is required",
-    }),
-  })
-})
-
 export const deleteMessageParams = object({
   params: object({
     messageId: string({
@@ -29,6 +21,5 @@ export const updateMessagePayload = object({
   })
 })
 
-export type ChatInput = TypeOf<typeof chatParams>
 export type DeleteMessageInput = TypeOf<typeof deleteMessageParams>
 export type UpdateMessageInput = TypeOf<typeof updateMessagePayload>
